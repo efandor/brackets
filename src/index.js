275b458e8e0b93d806a9f78664a78b7f839cf78d
efandor <firstdir@gmail.com>
2020-10-18 11:53:56 +0300

    feat: add solution to brackets task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  for (let i = 0; i < bracketsConfig.length;) {
+    brackets = bracketsConfig[i][0] + bracketsConfig[i][1];
+    if (str.includes(brackets)) {
+        str = str.replace(brackets, '');
+        i = 0;
+    } else i ++;
+  }
+return str === '' ? true : false
 }
+
